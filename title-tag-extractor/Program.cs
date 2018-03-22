960f4145bfd18c7d0d5d337a7c09d0ef9b38073c
Karel Tamayo <ktamayo@velocitypartners.net>
2018-03-22 00:21:51 -0300

    Add tabular display. Truncate long rows.

--- title-tag-extractor/Program.cs
+++ title-tag-extractor/Program.cs
@@ -15,22 +15,28 @@ namespace TitleTagExtractor
         private static void Main(string[] args)
         {
             //NOTE: To debug
+#if DEBUG
             args = new[]{
                 //"-h",
                 "-s", //show file names
-                "-f", //flatten results
+                "-f", //flatten results,
+                "-d", //display empty rows
                 @"src:C:\dev\working\transformer\samples",
                 "column-width:30",
                 "xpaths:(//Synopsis/TypeCode[text()=\"LOGLN\" and ../LanguageCode=\"EN\"])/../*"
             };
+#endif
 
             new Program()
                 .Run(args);
 
             Console.WriteLine("\n");
+
+            if (Debugger.IsAttached)
+                Console.ReadLine();
         }
 
-        private void Run(string[] args)
+        private void Run(IEnumerable<string> args)
         {
             MainAsync(args.ToList()).GetAwaiter().GetResult();
         }
@@ -38,7 +44,8 @@ namespace TitleTagExtractor
         private async Task MainAsync(List<string> argsList)
         {
             Debugger.Break();
-            var (showFileName, displayEmptyRows, flattenResults, columnWidth, xPaths, src) = ParseFlags(argsList);
+
+            var (showFileName, displayEmptyRows, flattenResults, xPaths, src) = ParseFlags(argsList);
             if (src == null)
                 return;
 
@@ -58,8 +65,14 @@ namespace TitleTagExtractor
 
             foreach (var xPath in xPaths)
             {
-                var count = 0;
-                DrawLine("XPATH", xPath);
+                var queryResults = new QueryResults
+                {
+                    Xpath = xPath,
+                    ShowFileNames = showFileName,
+                    DisplayEmptyRows = displayEmptyRows,
+                    FlattenResults = flattenResults
+                };
+
                 foreach (var file in sampleFiles)
                 {
                     try
@@ -70,88 +83,37 @@ namespace TitleTagExtractor
                             .GroupBy(x => x.Name)
                             .ToList();
 
-                        List<List<XElement>> elements = new List<List<XElement>>();
-                        if (!elementGroups.Any() && !showFileName)
-                            continue;
-
-                        //write headers only for the first file
-                        var columnPadding = 0;
-                        var totalColumns = 0;
-                        if (count++ == 0)
-                        {
-                            var firstColumnHeader = showFileName ? new[] { "File Name" } : new string[0];
-                            var dataHeaders = elementGroups
+                        //add the headers only once. They're all the same for each xpath.
+                        if (queryResults.Headers == null)
+                            queryResults.Headers = elementGroups
                                 .Select(e => e.Key.LocalName.ToString())
                                 .ToArray();
-                            totalColumns = firstColumnHeader.Length + dataHeaders.Length;
-                            columnPadding = Console.BufferWidth / totalColumns - 1;
-                            var headers = firstColumnHeader
-                                .Concat(dataHeaders)
-                                .Select(x => x.PadRight(columnPadding));
 
-                            DrawLine("", string.Join("|", headers));
-                        }
+                        //add the file name
+                        queryResults.FileNames.Add(file.Name);
 
-                        if (flattenResults)
+                        var elements = elementGroups
+                            .Select(g => g.ToList())
+                            .ToList();
+
+                        if (!elementGroups.Any())
                         {
-                            elements.Add(new List<XElement>());
-
-                            //fill only the first element
-                            foreach (var group in elementGroups)
-                                //flat this column values
-                                if (group.Count() > 1)
-                                {
-                                    var value = string.Join(", ",
-                                        group.Where(g => !string.IsNullOrEmpty(g.Value)).Select(g => g.Value)
-                                            .Distinct());
-                                    var xElement = new XElement(group.Key)
-                                    {
-                                        Value = string.IsNullOrEmpty(value) ? " " : value
-                                    };
-                                    elements[0].Add(xElement);
-                                }
-                                else
-                                {
-                                    elements[0].Add(group.ElementAt(0));
-                                }
-
-                            List<string> columnValues = (showFileName ? new[] { file.Name } : new string[0])
-                                .Concat(elements[0].Select(e => e.Value))
-                                .Select(x => x.PadRight(columnPadding))
-                                .ToList();
-
-                            //Skip empty rows if the flag is set.
-                            if (columnValues
-                                    .Skip(showFileName ? 1 : 0)
-                                    .All(string.IsNullOrEmpty) && !displayEmptyRows)
-                                continue;
-
-                            Console.WriteLine($"{string.Join("|", columnValues)}");
+                            //add an empty matrix. This file has no matches.
+                            queryResults.Data.Add(new string[0, 0]);
+                            continue;
                         }
-                        else
-                        {
-                            foreach (var group in elementGroups)
-                                elements.Add(group.ToList());
 
-                            //Display the matrix
-                            var totalRows = elements.Count;
+                        //create the matrix for this file
+                        var totalRows = elements[0].Count;
+                        var totalColumns = elements.Count;
+                        var matrix = new string[totalRows, totalColumns];
 
-                            for (int i = 0; i < totalColumns; i++)
-                            {
-                                List<string> columnValues = (showFileName ? new[] { file.Name } : new string[0]).ToList();
+                        for (var i = 0; i < totalRows; i++)
+                            for (var j = 0; j < totalColumns; j++)
+                                matrix[i, j] = elements[j][i].Value;
 
-                                for (var j = 0; j < totalRows; j++)
-                                    columnValues.Add(elements[j][i].Value.PadRight(columnPadding));
-
-                                //Skip empty rows if the flag is set.
-                                if (columnValues
-                                        .Skip(showFileName ? 1 : 0)
-                                        .All(string.IsNullOrEmpty) && !displayEmptyRows)
-                                    continue;
-
-                                Console.WriteLine($"{string.Join("|", columnValues)}");
-                            }
-                        }
+                        //add this matrix (file results) to the results (xpath query)
+                        queryResults.Data.Add(matrix);
                     }
                     catch (Exception e)
                     {
@@ -159,18 +121,109 @@ namespace TitleTagExtractor
                     }
                 }
 
-                DrawLine("Total items", count, true);
+                PrintResults(queryResults, showFileName);
+
+                DrawLine("Total items", queryResults.Data.Count, true);
                 Console.WriteLine();
             }
         }
 
-        private (bool showFileName, bool displayEmptyRows, bool flattenResults, int columnWidth, string[] xPaths, string src) ParseFlags(List<string> argsList)
+        private static void PrintResults(QueryResults queryResults, bool showFileName)
+        {
+            DrawLine("XPATH", queryResults.Xpath);
+
+            //write headers only for the first file
+            var firstColumnHeader = showFileName ? new[] { "File Name" } : new string[0];
+            var totalColumns = firstColumnHeader.Length + queryResults.Headers.Length;
+            var columnPadding = Console.BufferWidth / totalColumns - 1;
+
+            var headers = firstColumnHeader
+                .Concat(queryResults.Headers)
+                .Select(x => x.PadRight(columnPadding))
+                .ToList();
+
+            foreach (var _ in headers)
+                queryResults.ColumnPaddings.Add(columnPadding);
+            if (showFileName)
+            {
+                //var maxFileNameLength = queryResults.FileNames.Max(fn => fn.Length);
+                //maxFileNameLength = Math.Min(maxFileNameLength, columnPadding);
+                queryResults.ColumnPaddings.Add(columnPadding);
+            }
+
+            DrawLine(content: string.Join("|", headers));
+
+            for (var i = 0; i < queryResults.Data.Count; i++)
+            {
+                var matrix = queryResults.Data[i];
+
+                var flattenedMatrix = new string[1, matrix.GetLength(1)];
+                if (queryResults.FlattenResults && matrix.GetLength(0) > 1)
+                {
+                    //flatten this matrix before print it.
+                    for (var col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        var colValues = new List<string>();
+                        for (var row = 0; row < matrix.GetLength(0); row++)
+                        {
+                            var value = matrix[row, col];
+                            if (!string.IsNullOrEmpty(value))
+                                colValues.Add(value);
+                        }
+
+                        flattenedMatrix[0, col] = string.Join(", ", colValues.Distinct());
+                    }
+                    matrix = flattenedMatrix;
+                }
+
+                PrintMatrix(matrix, queryResults, queryResults.FileNames[i]);
+            }
+        }
+
+        private static void PrintMatrix(string[,] matrix, QueryResults queryResults, string fileName)
+        {
+            if (matrix.GetLength(0) == 0 && queryResults.DisplayEmptyRows)
+            {
+                if (queryResults.ShowFileNames)
+                    Console.Write(fileName.PadRight(queryResults.ColumnPaddings.Last()));
+
+                for (var i = 0; i < queryResults.Headers.Length; i++)
+                    Console.Write($"|{"".PadRight(queryResults.ColumnPaddings[i])}");
+
+                //change the line for the time we print.
+                Console.WriteLine();
+            }
+            else
+            {
+                for (var i = 0; i < matrix.GetLength(0); i++)
+                {
+                    //Print the filename if the flag is set.
+                    if (queryResults.ShowFileNames)
+                        Console.Write(fileName.PadRight(queryResults.ColumnPaddings.Last()));
+
+                    //print all row items.
+                    for (var j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        var item = matrix[i, j];
+                        if (item.Length > queryResults.ColumnPaddings[j])
+                            item = item.Substring(0, queryResults.ColumnPaddings[j]);
+
+                        Console.Write($"|{item.PadRight(queryResults.ColumnPaddings[j])}");
+                    }
+
+                    //change the line for the next row.
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private (bool showFileName, bool displayEmptyRows, bool flattenResults, string[] xPaths, string src) ParseFlags(List<string> argsList)
         {
             var help = argsList.Contains("-h") || argsList.Contains("--help");
             if (help)
             {
                 ShowUsage();
-                return (false, false, false, 0, null, null);
+                return (false, false, false, null, null);
             }
 
             Console.WriteLine("\n" +
@@ -191,12 +244,11 @@ namespace TitleTagExtractor
             if (!flattenResults)
                 flattenResults = ProcessFlag(argsList, "-f", false);
 
-            var columnWidth = ProcessFlag(argsList, "column-width:", 80);
             var src = ProcessFlag(argsList, "src:", ".");
             var xPaths = ProcessArrayFlag<string>(argsList, "xpaths:");
 
             //return
-            return (showFileName, displayEmptyRows, flattenResults, columnWidth, xPaths, src);
+            return (showFileName, displayEmptyRows, flattenResults, xPaths, src);
         }
 
         private static void ShowUsage()
@@ -210,7 +262,6 @@ namespace TitleTagExtractor
                 "\n-d, --display-empty-rows                 Set this flag to display the rows even when no results are found." +
                 "\n-f, --flatten-results                    Set this flag to display the results flattened for each file. If a query has several results, they'll be displayed on a single line, separated by commas." +
                 "\nsrc:<SRC_DIR>                            The directory to read the xml files from. Defaults to Current Dir." +
-                "\ncolumn-width:<COLUMN_WITH>               The width in characters of each column in the results table. Defaults 50" +
                 "\nxpaths:<XPATH_EXP>[\\<XPATH_EXP>]         The XPAth expression (or expressions) to run over the xml files in the src dir. " +
                 "\n                                         You can pass one or several back-slash-separated expressions here." +
                 "\n                                         Example: //Title/Id|//Title/Name" +
@@ -218,7 +269,7 @@ namespace TitleTagExtractor
                 "\n");
         }
 
-        private static void DrawLine(string header, object content, bool lineOnTop = false)
+        private static void DrawLine(string header = null, object content = null, bool lineOnTop = false)
         {
             if (lineOnTop)
                 Console.Write("-".PadRight(Console.BufferWidth, '-'));
@@ -232,7 +283,7 @@ namespace TitleTagExtractor
                 Console.Write("-".PadRight(Console.BufferWidth, '-'));
         }
 
-        private static async Task<XDocument> GetXDocument(FileInfo file)
+        private static async Task<XDocument> GetXDocument(FileSystemInfo file)
         {
             var doc = XDocument.Parse(await File.ReadAllTextAsync(file.FullName));
 
@@ -299,5 +350,24 @@ namespace TitleTagExtractor
 
             return result.ToArray();
         }
+    }
+
+    internal class QueryResults
+    {
+        public string Xpath { get; set; }
+
+        public List<string> FileNames { get; } = new List<string>();
+
+        public string[] Headers { get; set; }
+
+        public List<string[,]> Data { get; } = new List<string[,]>();
+
+        public bool ShowFileNames { get; set; }
+
+        public List<int> ColumnPaddings { get; } = new List<int>();
+
+        public bool DisplayEmptyRows { get; set; }
+
+        public bool FlattenResults { get; set; }
     }
 }
